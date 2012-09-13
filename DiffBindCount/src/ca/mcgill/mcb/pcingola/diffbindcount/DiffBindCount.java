21a3d1f40cda0b66187e4fb834ea4af2d7cceaf7
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2012-09-13 22:50:57 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@218 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- DiffBindCount/src/ca/mcgill/mcb/pcingola/diffbindcount/DiffBindCount.java
+++ DiffBindCount/src/ca/mcgill/mcb/pcingola/diffbindcount/DiffBindCount.java
@@ -1,6 +1,7 @@
 package ca.mcgill.mcb.pcingola.diffbindcount;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.List;
 
 import net.sf.picard.util.Interval;
@@ -10,33 +11,54 @@ import net.sf.samtools.SAMFileReader;
 import net.sf.samtools.SAMRecord;
 import ca.mcgill.mcb.pcingola.fileIterator.SeqChangeBedFileIterator;
 import ca.mcgill.mcb.pcingola.interval.SeqChange;
-import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
-import ca.mcgill.mcb.pcingola.util.Gpr;
 import ca.mcgill.mcb.pcingola.util.Timer;
 
 /**
- * Count reads from SnpEff
+ * Count reads from a BAM file given intervals
  * 
  * @author pablocingolani
  */
-public class DiffBindCount extends SnpEff {
+public class DiffBindCount {
+
+	String samFileName;
+	String intervalsFileName;
+	IntervalList intList;
+	SAMFileReader samReader;
 
 	/**
 	 * Main command line
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String samFileName = Gpr.HOME + "/tbVitaminD/chipSeq/bam/Index_12_N_PLUS_H3K4m1_1h.t30l32.sorted.bam";
-		String intervalsFileName = Gpr.HOME + "/tbVitaminD/chipSeq/macs/peaks/xls/Index_12_N_PLUS_H3K4m1_1h_peaks.xls";
+		DiffBindCount diffBindCount = new DiffBindCount();
+		diffBindCount.parse(args);
+		diffBindCount.run();
+	}
 
-		// Open file
-		Timer.showStdErr("Reading sam file: " + samFileName);
-		SAMFileReader samReader = new SAMFileReader(new File(samFileName));
+	/**
+	 * Parse
+	 * @param args
+	 */
+	void parse(String[] args) {
+		if (args.length != 2) {
+			System.err.println("Usage: " + this.getClass().getSimpleName() + " readsFile intervalsFile");
+			System.err.println("\treadsFile : A file contianing the reads. Either BAM or SAM format.");
+			System.err.println("\tintervals : Intervals in a BED or XLS (MACs).");
+			System.exit(-1);
+		}
 
+		samFileName = args[0];
+		intervalsFileName = args[1];
+	}
+
+	/**
+	 * Read intervals from BED or XLS
+	 */
+	protected void readIntervals() {
 		// Create interval list
 		Timer.showStdErr("Reading intervals from: " + intervalsFileName);
 		int count = 0;
-		IntervalList intList = new IntervalList(samReader.getFileHeader());
+		intList = new IntervalList(samReader.getFileHeader());
 		SeqChangeBedFileIterator bed = new SeqChangeBedFileIterator(intervalsFileName);
 		for (SeqChange sc : bed) {
 			Interval interval = new Interval(sc.getChromosomeNameOri(), sc.getStart(), sc.getEnd());
@@ -45,18 +67,39 @@ public class DiffBindCount extends SnpEff {
 		}
 		Timer.showStdErr("Total " + count + " intervals added.");
 		if (count <= 0) throw new RuntimeException("No intervals, nothing to do!");
+	}
+
+	/**
+	 * Count reads
+	 */
+	public void run() {
+		// Open file
+		Timer.showStdErr("Reading sam file: " + samFileName);
+		samReader = new SAMFileReader(new File(samFileName));
+
+		// Read intervals from XLS or BED file
+		readIntervals();
 
 		// Create interval iterator
 		SamLocusIterator samLocusIter = new SamLocusIterator(samReader, intList, true);
-		count = 0;
+		int intervalNumber = -1;
+		Interval interval = null;
+		HashSet<String> reads = new HashSet<String>();
 		for (SamLocusIterator.LocusInfo locusInfo : samLocusIter) {
+			// Next interval in the list?
+			if ((interval == null) || (!interval.getSequence().equals(locusInfo.getSequenceName()) || ((interval.getStart() < locusInfo.getPosition()) && (interval.getEnd() > locusInfo.getPosition())))) {
+				if (interval != null) System.out.println(interval.getSequence() + "\t" + interval.getStart() + "\t" + interval.getEnd() + "\t" + reads.size());
+				intervalNumber++;
+				interval = intList.getIntervals().get(intervalNumber);
+				reads = new HashSet<String>();
+			}
+
+			// Get all reads form this position
 			List<SamLocusIterator.RecordAndOffset> recAndPosList = locusInfo.getRecordAndPositions();
-			System.out.println("LOCUS INFO: " + locusInfo + "\tSeqName:" + locusInfo.getSequenceName() + "\tPos:" + locusInfo.getPosition());
 			for (SamLocusIterator.RecordAndOffset recAndPos : recAndPosList) {
 				SAMRecord samRec = recAndPos.getRecord();
-				System.out.println("\t" + samRec + "\t" + samRec.getReferenceName() + "\t" + samRec.getAlignmentStart() + "\t" + samRec.getAlignmentEnd() + "\t" + (samRec.getAlignmentEnd() - samRec.getAlignmentStart()));
+				reads.add(samRec.getReadName());
 			}
-			if (count++ > 100) throw new RuntimeException("FORCD END!");
 		}
 
 		samLocusIter.close();
