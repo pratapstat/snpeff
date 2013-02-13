324232d783fe9abb0db3e1504bb704f78ef02619
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2013-02-13 15:48:46 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@426 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/SnpSiftCmdSplit.java
+++ SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/SnpSiftCmdSplit.java
@@ -21,6 +21,7 @@ public class SnpSiftCmdSplit extends SnpSift {
 
 	String vcfFile;
 	StringBuilder header = new StringBuilder();
+	int numLines;
 
 	public SnpSiftCmdSplit(String args[]) {
 		super(args, "split");
@@ -32,10 +33,17 @@ public class SnpSiftCmdSplit extends SnpSift {
 	 * @param chr
 	 * @return
 	 */
-	BufferedWriter newFile(String baseName, String chr) {
-		String outFileName = baseName + "." + chr + ".vcf";
-		System.err.println("");
-		Timer.showStdErr("Creating new file '" + outFileName + "'");
+	BufferedWriter newFile(String baseName, String chr, int fileNumber) {
+
+		// File name
+		String outFileName = "";
+		if (numLines <= 0) outFileName = baseName + "." + chr + ".vcf"; // Splitting by chromosome
+		else outFileName = String.format("%s.%03d.vcf", baseName, fileNumber); // Splitting by number of lines
+
+		if (verbose) {
+			System.err.println("");
+			Timer.showStdErr("Creating new file '" + outFileName + "'");
+		}
 
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(outFileName));
@@ -51,11 +59,21 @@ public class SnpSiftCmdSplit extends SnpSift {
 	 */
 	@Override
 	public void parse(String[] args) {
-		int argNum = 0;
+		numLines = -1;
 		if (args.length == 0) usage(null);
 
-		if (args.length >= argNum) vcfFile = args[argNum++];
-		else usage("Missing 'file.vcf'");
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-l")) {
+				// Parse number of lines
+				if ((i + 1) < args.length) {
+					String numStr = args[++i];
+					numLines = Gpr.parseIntSafe(numStr);
+					if (numLines <= 0) usage("Number of lines must be a positive number. num = '" + numStr + "'");
+				} else usage("Missing 'num' argument");
+			} else if (vcfFile == null) vcfFile = args[i];
+		}
+
+		if (vcfFile == null) usage("Missing 'file.vcf'");
 	}
 
 	/**
@@ -70,7 +88,7 @@ public class SnpSiftCmdSplit extends SnpSift {
 
 		// Init
 		boolean isHeader = true;
-		int lineNum = 1;
+		int lineNum = 1, fileNum = 0;
 		String chrPrev = "";
 
 		// Open file and iterate
@@ -95,11 +113,17 @@ public class SnpSiftCmdSplit extends SnpSift {
 					String fields[] = line.split("\t", 2);
 					String chr = fields[0];
 
-					// New chromosome?
-					if (!chr.equals(chrPrev)) {
+					// New file? Split file?
+					if ((out == null) // No file created?
+							|| ((numLines > 0) && (numLines <= lineNum)) // Splitting by lines :  Number of lines reached?
+							|| ((numLines <= 0) && (!chr.equals(chrPrev))) // Splitting by chromosome split: Is this a new chromosome? 
+					) {
+						// Split file
 						if (out != null) out.close();
-						out = newFile(baseName, chr);
+						out = newFile(baseName, chr, fileNum);
+						fileNum++;
 						chrPrev = chr;
+						lineNum = 1;
 					}
 
 					// Write line
@@ -130,10 +154,9 @@ public class SnpSiftCmdSplit extends SnpSift {
 		}
 
 		showVersion();
-
 		System.err.println("Usage: java -jar " + SnpSift.class.getSimpleName() + ".jar splitChr [-v] [-l <num>]file.vcf");
 		System.err.println("Options:");
-		System.err.println("\t-l <num>   : Split by number of lines instead of chromosomes.");
+		System.err.println("\t-l <num>   : Split by 'num' lines (instead of chromosomes).");
 		System.exit(1);
 	}
 }
