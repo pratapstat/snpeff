2ea6cc695be9f62ccfefb92bbdbc30a1828e7674
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2013-02-27 19:45:02 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@475 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesCaseControl.java
+++ SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesCaseControl.java
@@ -20,7 +20,19 @@ public class TestCasesCaseControl extends TestCase {
 
 	public static boolean debug = false;
 
-	void checkCaseControl(String vcfFile, String tfamFile, String casesStr, String controlStr) {
+	void checkCaseControlString(String vcfFile, String geoupStr, String casesStr, String controlStr) {
+		String args[] = { geoupStr, vcfFile };
+		SnpSiftCmdCaseControl cmd = new SnpSiftCmdCaseControl(args);
+
+		List<VcfEntry> vcfEntries = cmd.run(true);
+		for (VcfEntry ve : vcfEntries) {
+			System.out.println(ve);
+			Assert.assertEquals(casesStr, ve.getInfo(SnpSiftCmdCaseControl.VCF_INFO_CASE));
+			Assert.assertEquals(controlStr, ve.getInfo(SnpSiftCmdCaseControl.VCF_INFO_CONTROL));
+		}
+	}
+
+	void checkCaseControlTfam(String vcfFile, String tfamFile, String casesStr, String controlStr) {
 		String args[] = { "-tfam", tfamFile, vcfFile };
 		SnpSiftCmdCaseControl cmd = new SnpSiftCmdCaseControl(args);
 
@@ -33,35 +45,55 @@ public class TestCasesCaseControl extends TestCase {
 	}
 
 	public void test_01() {
-		checkCaseControl("test/test.private.01.vcf", "test/test.private.01.tfam", "0,0,0", "0,0,0");
+		checkCaseControlTfam("test/test.private.01.vcf", "test/test.private.01.tfam", "0,0,0", "0,0,0");
+	}
+
+	public void test_01_Str() {
+		checkCaseControlString("test/test.private.01.vcf", "--", "0,0,0", "0,0,0");
 	}
 
 	public void test_02() {
-		checkCaseControl("test/test.private.02.vcf", "test/test.private.01.tfam", "0,0,0", "2,0,4");
+		checkCaseControlTfam("test/test.private.02.vcf", "test/test.private.01.tfam", "0,0,0", "2,0,4");
+	}
+
+	public void test_02_Str() {
+		checkCaseControlString("test/test.private.02.vcf", "--", "0,0,0", "2,0,4");
+	}
+
+	public void test_02_Str2() {
+		checkCaseControlString("test/test.private.02.vcf", "-+", "1,0,2", "1,0,2");
 	}
 
 	public void test_03() {
-		checkCaseControl("test/test.private.03.vcf", "test/test.private.01.tfam", "0,0,0", "0,1,1");
+		checkCaseControlTfam("test/test.private.03.vcf", "test/test.private.01.tfam", "0,0,0", "0,1,1");
+	}
+
+	public void test_03_Str() {
+		checkCaseControlString("test/test.private.03.vcf", "--", "0,0,0", "0,1,1");
 	}
 
 	public void test_04() {
-		checkCaseControl("test/test.private.04.vcf", "test/test.private.01.tfam", "0,0,0", "1,0,2");
+		checkCaseControlTfam("test/test.private.04.vcf", "test/test.private.01.tfam", "0,0,0", "1,0,2");
+	}
+
+	public void test_04_Str() {
+		checkCaseControlString("test/test.private.04.vcf", "--", "0,0,0", "1,0,2");
 	}
 
 	public void test_05() {
-		checkCaseControl("test/test.private.05.vcf", "test/test.private.05.tfam", "0,0,0", "0,0,0");
+		checkCaseControlTfam("test/test.private.05.vcf", "test/test.private.05.tfam", "0,0,0", "0,0,0");
 	}
 
 	public void test_06() {
-		checkCaseControl("test/test.private.06.vcf", "test/test.private.05.tfam", "0,1,1", "0,0,0");
+		checkCaseControlTfam("test/test.private.06.vcf", "test/test.private.05.tfam", "0,1,1", "0,0,0");
 	}
 
 	public void test_07() {
-		checkCaseControl("test/test.private.07.vcf", "test/test.private.05.tfam", "0,1,1", "0,1,1");
+		checkCaseControlTfam("test/test.private.07.vcf", "test/test.private.05.tfam", "0,1,1", "0,1,1");
 	}
 
 	public void test_08() {
-		checkCaseControl("test/test.private.08.vcf", "test/test.private.05.tfam", "0,2,2", "0,1,1");
+		checkCaseControlTfam("test/test.private.08.vcf", "test/test.private.05.tfam", "0,2,2", "0,1,1");
 	}
 
 }
