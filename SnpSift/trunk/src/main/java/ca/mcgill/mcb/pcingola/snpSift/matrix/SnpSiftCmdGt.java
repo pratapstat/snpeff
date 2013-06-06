81b3e219cbcca1d94fbfbe7e689c8509561baa65
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2013-06-06 23:30:21 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@704 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/matrix/SnpSiftCmdGt.java
+++ SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/matrix/SnpSiftCmdGt.java
@@ -6,7 +6,6 @@ import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
 import ca.mcgill.mcb.pcingola.snpSift.SnpSift;
 import ca.mcgill.mcb.pcingola.util.Gpr;
 import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
-import ca.mcgill.mcb.pcingola.vcf.VcfGenotype;
 
 /**
  * Add genotype information to INFO fields
@@ -14,10 +13,6 @@ import ca.mcgill.mcb.pcingola.vcf.VcfGenotype;
  * @author pcingola
  */
 public class SnpSiftCmdGt extends SnpSift {
-
-	public static final String VCF_INFO_HOMS = "HO";
-	public static final String VCF_INFO_HETS = "HE";
-	public static final String VCF_INFO_NAS = "NA";
 
 	public static int SHOW_EVERY = 100;
 	String vcfFile;
@@ -36,51 +31,14 @@ public class SnpSiftCmdGt extends SnpSift {
 	@Override
 	protected List<String> addHeader() {
 		List<String> newHeaders = super.addHeader();
-		newHeaders.add("##INFO=<ID=" + VCF_INFO_HOMS + ",Number=.,Type=Integer,Description=\"List of sample indexes having homozygous ALT genotypes\">");
-		newHeaders.add("##INFO=<ID=" + VCF_INFO_HETS + ",Number=.,Type=Integer,Description=\"List of sample indexes having heterozygous ALT genotypes\">");
-		newHeaders.add("##INFO=<ID=" + VCF_INFO_NAS + ",Number=.,Type=Integer,Description=\"List of sample indexes having missing genotypes\">");
+		newHeaders.add("##INFO=<ID=" + VcfEntry.VCF_INFO_HOMS + ",Number=.,Type=Integer,Description=\"List of sample indexes having homozygous ALT genotypes\">");
+		newHeaders.add("##INFO=<ID=" + VcfEntry.VCF_INFO_HETS + ",Number=.,Type=Integer,Description=\"List of sample indexes having heterozygous ALT genotypes\">");
+		newHeaders.add("##INFO=<ID=" + VcfEntry.VCF_INFO_NAS + ",Number=.,Type=Integer,Description=\"List of sample indexes having missing genotypes\">");
 		newHeaders.add("##SnpSiftCmd=\"" + commandLineStr() + "\"");
 		return newHeaders;
 	}
 
-	/**
-	 * Process a VCF entry and return a string (tab separated values)
-	 * @param vcfEntry
-	 * @return
-	 */
-	public boolean compressEntry(VcfEntry vcfEntry) {
-		if (vcfEntry.getAlts().length > 1) return false;
-
-		StringBuilder homs = new StringBuilder();
-		StringBuilder hets = new StringBuilder();
-		StringBuilder nas = new StringBuilder();
-
-		// Add all genotype codes
-		int idx = 0;
-		for (VcfGenotype gen : vcfEntry.getVcfGenotypes()) {
-			int score = gen.getGenotypeCode();
-
-			if (score == 0) {
-				; //Nothing to do
-			} else if (score < 0) nas.append((nas.length() > 0 ? "," : "") + idx);
-			else if (score == 1) hets.append((hets.length() > 0 ? "," : "") + idx);
-			else if (score == 2) homs.append((homs.length() > 0 ? "," : "") + idx);
-			else {
-				Gpr.debug("SCORE: " + score + "\t" + gen);
-				return false;
-			}
-
-			idx++;
-		}
-
-		// Update INFO fields
-		if (homs.length() > 0) vcfEntry.addInfo(VCF_INFO_HOMS, homs.toString());
-		if (hets.length() > 0) vcfEntry.addInfo(VCF_INFO_HETS, hets.toString());
-		if (nas.length() > 0) vcfEntry.addInfo(VCF_INFO_NAS, nas.toString());
-
-		return true;
-	}
-
+	@Override
 	public String getOutput() {
 		return output.toString();
 	}
@@ -101,28 +59,6 @@ public class SnpSiftCmdGt extends SnpSift {
 		}
 	}
 
-	/**
-	 * Parse genotype string (sparse matrix) and set all entries using 'value'
-	 * 
-	 * @param str
-	 * @param gt
-	 * @param value
-	 */
-	void parseSparseGt(String str, byte gt[], int valueInt) {
-		if (str == null) return;
-
-		// Split comma separated indeces
-		String idxs[] = str.split(",");
-		byte value = (byte) valueInt;
-
-		// Set all entries
-		for (String idx : idxs) {
-			int i = Gpr.parseIntSafe(idx);
-			gt[i] = value;
-		}
-
-	}
-
 	void print(String out) {
 		if (save) output.append(out + "\n");
 		else System.out.println(out);
@@ -138,21 +74,20 @@ public class SnpSiftCmdGt extends SnpSift {
 		VcfFileIterator vcf = new VcfFileIterator(vcfFile);
 		if (save) showHeader = false; // No need to show header
 
-		int i = 1, numSamples = 0;
+		int i = 1;
 		for (VcfEntry ve : vcf) {
 			if (vcf.isHeadeSection()) {
 				handleVcfHeader(vcf);
-				numSamples = vcf.getVcfHeader().getSampleNames().size();
 
 				if (save) print(vcf.getVcfHeader().toString()); // Save header to output buffer
 			}
 
 			if (uncompress) {
 				// Uncompress
-				print(uncompressEntry(ve, numSamples));
+				print(ve.uncompressGenotypes().toString());
 			} else {
 				// Compress
-				if (compressEntry(ve)) print(ve.toStringNoGt());
+				if (ve.compressGenotypes()) print(ve.toStringNoGt());
 				else print(ve.toString());
 			}
 
@@ -162,56 +97,6 @@ public class SnpSiftCmdGt extends SnpSift {
 
 	public void setSave(boolean save) {
 		this.save = save;
-	}
-
-	public String uncompressEntry(VcfEntry vcfEntry, int numSamples) {
-		// Get 'sparse' matrix entries
-		String hoStr = vcfEntry.getInfo(VCF_INFO_HOMS);
-		String heStr = vcfEntry.getInfo(VCF_INFO_HETS);
-		String naStr = vcfEntry.getInfo(VCF_INFO_NAS);
-
-		// Has genotype field? No compression
-		if (vcfEntry.hasGenotypes()) return vcfEntry.toString();
-
-		// Parse 'sparse' entries
-		byte gt[] = new byte[numSamples];
-		parseSparseGt(naStr, gt, -1);
-		parseSparseGt(heStr, gt, 1);
-		parseSparseGt(hoStr, gt, 2);
-
-		// Remove info fields
-		if (hoStr != null) vcfEntry.rmInfo(VCF_INFO_HOMS);
-		if (heStr != null) vcfEntry.rmInfo(VCF_INFO_HETS);
-		if (naStr != null) vcfEntry.rmInfo(VCF_INFO_NAS);
-		vcfEntry.setFormat("GT");
-
-		// Create output string 
-		StringBuilder out = new StringBuilder();
-		out.append(vcfEntry.toStringNoGt());
-		out.append("\t" + vcfEntry.getFormat());
-
-		for (int i = 0; i < gt.length; i++)
-			switch (gt[i]) {
-			case -1:
-				out.append("\t./.");
-				break;
-
-			case 0:
-				out.append("\t0/0");
-				break;
-
-			case 1:
-				out.append("\t0/1");
-				break;
-
-			case 2:
-				out.append("\t1/1");
-				break;
-
-			default:
-				throw new RuntimeException("Unknown code '" + gt[i] + "'");
-			}
-		return out.toString();
 	}
 
 	@Override
