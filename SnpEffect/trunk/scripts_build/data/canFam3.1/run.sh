2a501c166a2f0289cae3f48b4da71e305cd244d5
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2013-09-20 16:15:06 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@957 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpEffect/trunk/scripts_build/data/canFam3.1/run.sh
+++ SnpEffect/trunk/scripts_build/data/canFam3.1/run.sh
@@ -57,50 +57,64 @@ echo "Download data"
 echo "Build reference FASTA file"
 #---
 
-# zcat  \
-# 		cfa_ref_CanFam3.1_chr1.fa.gz \
-# 		cfa_ref_CanFam3.1_chr2.fa.gz \
-# 		cfa_ref_CanFam3.1_chr3.fa.gz \
-# 		cfa_ref_CanFam3.1_chr4.fa.gz \
-# 		cfa_ref_CanFam3.1_chr5.fa.gz \
-# 		cfa_ref_CanFam3.1_chr6.fa.gz \
-# 		cfa_ref_CanFam3.1_chr7.fa.gz \
-# 		cfa_ref_CanFam3.1_chr8.fa.gz \
-# 		cfa_ref_CanFam3.1_chr9.fa.gz \
-# 		cfa_ref_CanFam3.1_chr10.fa.gz \
-# 		cfa_ref_CanFam3.1_chr11.fa.gz \
-# 		cfa_ref_CanFam3.1_chr12.fa.gz \
-# 		cfa_ref_CanFam3.1_chr13.fa.gz \
-# 		cfa_ref_CanFam3.1_chr14.fa.gz \
-# 		cfa_ref_CanFam3.1_chr15.fa.gz \
-# 		cfa_ref_CanFam3.1_chr16.fa.gz \
-# 		cfa_ref_CanFam3.1_chr17.fa.gz \
-# 		cfa_ref_CanFam3.1_chr18.fa.gz \
-# 		cfa_ref_CanFam3.1_chr19.fa.gz \
-# 		cfa_ref_CanFam3.1_chr20.fa.gz \
-# 		cfa_ref_CanFam3.1_chr21.fa.gz \
-# 		cfa_ref_CanFam3.1_chr22.fa.gz \
-# 		cfa_ref_CanFam3.1_chr23.fa.gz \
-# 		cfa_ref_CanFam3.1_chr24.fa.gz \
-# 		cfa_ref_CanFam3.1_chr25.fa.gz \
-# 		cfa_ref_CanFam3.1_chr26.fa.gz \
-# 		cfa_ref_CanFam3.1_chr27.fa.gz \
-# 		cfa_ref_CanFam3.1_chr28.fa.gz \
-# 		cfa_ref_CanFam3.1_chr29.fa.gz \
-# 		cfa_ref_CanFam3.1_chr30.fa.gz \
-# 		cfa_ref_CanFam3.1_chr31.fa.gz \
-# 		cfa_ref_CanFam3.1_chr32.fa.gz \
-# 		cfa_ref_CanFam3.1_chr33.fa.gz \
-# 		cfa_ref_CanFam3.1_chr34.fa.gz \
-# 		cfa_ref_CanFam3.1_chr35.fa.gz \
-# 		cfa_ref_CanFam3.1_chr36.fa.gz \
-# 		cfa_ref_CanFam3.1_chr37.fa.gz \
-# 		cfa_ref_CanFam3.1_chr38.fa.gz \
-# 		cfa_ref_CanFam3.1_chrMT.fa.gz \
-# 		cfa_ref_CanFam3.1_chrX.fa.gz \
-# 		cfa_ref_CanFam3.1_unplaced.fa.gz \
-# 	| sed "s/^>gi|[0-9]*|ref|\(.*\..*\)|.*/>\1/" \
-# 	> ../../genomes/canFam3.1.fa
+# Create 'chr.sh' script to change chromosome names
+echo "#!/bin/sh" > chr.sh
+echo >> chr.sh
+echo "cat \\" >> chr.sh
+
+for fa in cfa_ref_CanFam3.1_chr*.fa.gz
+do
+	chr=`echo $fa | cut -f 2 -d . | cut -f 2 -d _`
+	chrName=`zcat $fa | head -n 1 | sed "s/^>gi|[0-9]*|ref|\(.*\..*\)|.*/\1/"`
+	echo "    | sed \"s/$chrName/$chr/g\" \\" >> chr.sh
+done
+chmod a+x chr.sh
+
+zcat  \
+		cfa_ref_CanFam3.1_chr1.fa.gz \
+		cfa_ref_CanFam3.1_chr2.fa.gz \
+		cfa_ref_CanFam3.1_chr3.fa.gz \
+		cfa_ref_CanFam3.1_chr4.fa.gz \
+		cfa_ref_CanFam3.1_chr5.fa.gz \
+		cfa_ref_CanFam3.1_chr6.fa.gz \
+		cfa_ref_CanFam3.1_chr7.fa.gz \
+		cfa_ref_CanFam3.1_chr8.fa.gz \
+		cfa_ref_CanFam3.1_chr9.fa.gz \
+		cfa_ref_CanFam3.1_chr10.fa.gz \
+		cfa_ref_CanFam3.1_chr11.fa.gz \
+		cfa_ref_CanFam3.1_chr12.fa.gz \
+		cfa_ref_CanFam3.1_chr13.fa.gz \
+		cfa_ref_CanFam3.1_chr14.fa.gz \
+		cfa_ref_CanFam3.1_chr15.fa.gz \
+		cfa_ref_CanFam3.1_chr16.fa.gz \
+		cfa_ref_CanFam3.1_chr17.fa.gz \
+		cfa_ref_CanFam3.1_chr18.fa.gz \
+		cfa_ref_CanFam3.1_chr19.fa.gz \
+		cfa_ref_CanFam3.1_chr20.fa.gz \
+		cfa_ref_CanFam3.1_chr21.fa.gz \
+		cfa_ref_CanFam3.1_chr22.fa.gz \
+		cfa_ref_CanFam3.1_chr23.fa.gz \
+		cfa_ref_CanFam3.1_chr24.fa.gz \
+		cfa_ref_CanFam3.1_chr25.fa.gz \
+		cfa_ref_CanFam3.1_chr26.fa.gz \
+		cfa_ref_CanFam3.1_chr27.fa.gz \
+		cfa_ref_CanFam3.1_chr28.fa.gz \
+		cfa_ref_CanFam3.1_chr29.fa.gz \
+		cfa_ref_CanFam3.1_chr30.fa.gz \
+		cfa_ref_CanFam3.1_chr31.fa.gz \
+		cfa_ref_CanFam3.1_chr32.fa.gz \
+		cfa_ref_CanFam3.1_chr33.fa.gz \
+		cfa_ref_CanFam3.1_chr34.fa.gz \
+		cfa_ref_CanFam3.1_chr35.fa.gz \
+		cfa_ref_CanFam3.1_chr36.fa.gz \
+		cfa_ref_CanFam3.1_chr37.fa.gz \
+		cfa_ref_CanFam3.1_chr38.fa.gz \
+		cfa_ref_CanFam3.1_chrMT.fa.gz \
+		cfa_ref_CanFam3.1_chrX.fa.gz \
+		cfa_ref_CanFam3.1_unplaced.fa.gz \
+	| sed "s/^>gi|[0-9]*|ref|\(.*\..*\)|.*/>\1/" \
+	| ./chr.sh \
+	> ../../genomes/canFam3.1.fa
 
 #---
 echo "Build GFF file"
@@ -108,6 +122,7 @@ echo "Build GFF file"
 
 zcat ref_CanFam3.1_top_level.gff3.gz \
 	| ../fixGff.pl \
+	| ./chr.sh \
 	> ../genes.gff
 
 cat ../genes.gff \
@@ -130,12 +145,11 @@ zcat protein.fa.gz \
 	| ../cdsId_trId.pl \
 	> ../protein.fa
 
-
 #---
 echo "Build SnpEff database"
 #---
 
 cd ../../..
-java -Xmx10G -jar snpEff.jar build -v -gff3 canFam3.1
+java -Xmx10G -jar snpEff.jar build -v -gff3 canFam3.1 2>&1 | tee canFam3.1.build
 java -Xmx10G -jar snpEff.jar dump canFam3.1 > canFam3.1.dump
 
