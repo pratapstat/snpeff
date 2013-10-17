24ef2e5dcccd881a88ffb1e8bd643112fab80a8f
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2013-10-17 00:11:45 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@980 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpEffect/trunk/scripts_build/download_hg19.sh
+++ SnpEffect/trunk/scripts_build/download_hg19.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 #-------------------------------------------------------------------------------
 #
@@ -17,78 +17,80 @@
 #-------------------------------------------------------------------------------
 
 REF=hg19
+
+mkdir -p $REF || true
 cd data/$REF/
 
 #---
 # Download latest datasets
 #---
 
-# # Genome sequence
-# wget http://hgdownload.cse.ucsc.edu/goldenPath/$REF/bigZips/chromFa.tar.gz
-# 
-# # Protein sequences
-# wget ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.protein.faa.gz
-# 
-# # CDS sequences
-# wget ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.rna.fna.gz
-# 
-# # RefLink
-# wget http://hgdownload.cse.ucsc.edu/goldenPath/$REF/database/refLink.txt.gz
-# gunzip refLink.txt.gz
-# 
+# Genome sequence
+wget http://hgdownload.cse.ucsc.edu/goldenPath/$REF/bigZips/chromFa.tar.gz
+
+# Protein sequences
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.protein.faa.gz
+
+# CDS sequences
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.rna.fna.gz
+
+# RefLink
+wget http://hgdownload.cse.ucsc.edu/goldenPath/$REF/database/refLink.txt.gz
+gunzip refLink.txt.gz
+
 #---
 # Create FASTA file
 #---
-# rm -rvf chr
-# mkdir chr 
-# cd chr
-# tar -xvzf ../chromFa.tar.gz
-# 
-# FASTA=../$REF.fa
-# echo Creating FASTA file
-# rm -vf $FASTA
-# cat chr[1-9].fa    >> $FASTA
-# cat chr??.fa   >> $FASTA
-# cat chr[A-Z].fa    >> $FASTA
-# cat chr???*.fa >> $FASTA
-# 
-# cd -
-# 
-# # Compress genome file
-# $HOME/tools/pigz/pigz hg19.fa
-# cp hg19.fa.gz $HOME/snpEff/data/genomes/
+rm -rvf chr
+mkdir chr 
+cd chr
+tar -xvzf ../chromFa.tar.gz
+
+FASTA=../$REF.fa
+echo Creating FASTA file
+rm -vf $FASTA
+cat chr[1-9].fa    >> $FASTA
+cat chr??.fa   >> $FASTA
+cat chr[A-Z].fa    >> $FASTA
+cat chr???*.fa >> $FASTA
+
+cd -
+
+# Compress genome file
+$HOME/tools/pigz/pigz hg19.fa
+cp hg19.fa.gz $HOME/snpEff/data/genomes/
 
 #---
 # Download Gene information
 #---
 
-# echo "Query MySql database"
-# 
-# (
-# echo "use hg19;"
-# echo "select rg.bin as '#bin'"
-# echo "		, CONCAT(rg.name,'.',gi.version) as 'name'"
-# echo "		, rg.chrom, rg.strand"
-# echo "		, rg.txStart"
-# echo "		, rg.txEnd"
-# echo "		, rg.cdsStart"
-# echo "		, rg.cdsEnd"
-# echo "		, rg.exonCount"
-# echo "		, rg.exonStarts"
-# echo "		, rg.exonEnds"
-# echo "		, rg.score"
-# echo "		, rg.name2"
-# echo "		, rg.cdsStartStat"
-# echo "		, rg.cdsEndStat"
-# echo "		, rg.exonFrames"
-# echo "	from refGene rg"
-# echo "	inner join gbCdnaInfo gi"
-# echo "		on rg.name=gi.acc"
-# echo ";"
-# ) | mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A hg19 > genes.txt
-# 
-# # Compress file
-# gzip genes.txt
+echo "Query MySql database"
+
+(
+echo "use hg19;"
+echo "select rg.bin as '#bin'"
+echo "		, CONCAT(rg.name,'.',gi.version) as 'name'"
+echo "		, rg.chrom, rg.strand"
+echo "		, rg.txStart"
+echo "		, rg.txEnd"
+echo "		, rg.cdsStart"
+echo "		, rg.cdsEnd"
+echo "		, rg.exonCount"
+echo "		, rg.exonStarts"
+echo "		, rg.exonEnds"
+echo "		, rg.score"
+echo "		, rg.name2"
+echo "		, rg.cdsStartStat"
+echo "		, rg.cdsEndStat"
+echo "		, rg.exonFrames"
+echo "	from refGene rg"
+echo "	inner join gbCdnaInfo gi"
+echo "		on rg.name=gi.acc"
+echo ";"
+) | mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A hg19 > genes.txt
+
+# Compress file
+gzip genes.txt
 
 #---
 # Create CDS and protein files
