2c3a0a5de72dd56331aece924997f5b9eb76328d
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2013-11-04 19:48:07 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@1027 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpEffect/trunk/src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
+++ SnpEffect/trunk/src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
@@ -1,19 +1,52 @@
 package ca.mcgill.mcb.pcingola;
 
-import ca.mcgill.mcb.pcingola.probablility.FisherExactTest;
+import java.util.List;
+
+import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
+import ca.mcgill.mcb.pcingola.interval.SeqChange;
+import ca.mcgill.mcb.pcingola.snpEffect.ChangeEffect;
+import ca.mcgill.mcb.pcingola.snpEffect.Config;
+import ca.mcgill.mcb.pcingola.snpEffect.SnpEffectPredictor;
+import ca.mcgill.mcb.pcingola.util.Gpr;
+import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
 public class Zzz {
 
 	public static void main(String[] args) {
-		int k = 0;
-		int N = 12791;
-		int D = 6385;
-		int n = 0;
-		double pup = FisherExactTest.get().fisherExactTestUp(k, N, D, n);
-		double pdown = FisherExactTest.get().fisherExactTestDown(k, N, D, n);
+		String genome = "GRCh37.71";
+		genome = "testHg3771Chr1";
+		String vcfFileName = Gpr.HOME + "/snpEff/test.vcf";
 
-		System.out.println("Up: " + pup);
-		System.out.println("Down: " + pdown);
+		// Load config file
+		System.out.println("Loading config file");
+		String configFile = Config.DEFAULT_CONFIG_FILE;
+		Config config = new Config(genome, configFile);
+
+		// Load database
+		System.out.println("Loading database");
+		SnpEffectPredictor snpEffectPredictor = config.loadSnpEffectPredictor();
+
+		// Build data structures (interval forest)
+		System.out.println("Building forest");
+		snpEffectPredictor.buildForest();
+
+		// Iterate on VCF
+		System.out.println("Reading VCF file");
+		VcfFileIterator vcf = new VcfFileIterator(vcfFileName);
+		for (VcfEntry vcfEntry : vcf) {
+			// Show line
+			System.out.println(vcfEntry.toStringNoGt());
+
+			// Iterate on each 'sequence change' for each VCF entry
+			for (SeqChange seqChange : vcfEntry.seqChanges()) {
+				// Calculate effects
+				List<ChangeEffect> effects = snpEffectPredictor.seqChangeEffect(seqChange);
+
+				// Show all effects
+				for (ChangeEffect changeEffect : effects)
+					System.out.println("\t" + changeEffect);
+			}
+		}
 	}
 
 }
