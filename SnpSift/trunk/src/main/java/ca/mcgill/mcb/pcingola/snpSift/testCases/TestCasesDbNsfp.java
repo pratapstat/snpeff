452c764b28434786a65228b589fe512b39f9759b
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2013-11-14 21:42:27 +0000

    DbNsfp issue fixed.
    git-svn-id: https://svn.code.sf.net/p/snpeff/code@1064 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesDbNsfp.java
+++ SnpSift/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesDbNsfp.java
@@ -1,7 +1,9 @@
 package ca.mcgill.mcb.pcingola.snpSift.testCases;
 
-import junit.framework.Assert;
 import junit.framework.TestCase;
+
+import org.junit.Assert;
+
 import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
 import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdAnnotateSortedDbNsfp;
 import ca.mcgill.mcb.pcingola.util.Gpr;
@@ -15,12 +17,14 @@ import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 public class TestCasesDbNsfp extends TestCase {
 
 	public static boolean verbose = false;
+	public static boolean debug = false;
 
 	public void test_01() {
 		String vcfFileName = "test/test_dbNSFP_chr1_69134.vcf";
-		String args[] = { "test/dbNSFP2.0b3.chr1_69134.txt", "test/test_dbNSFP_chr1_69134.vcf" };
+		String args[] = { "test/dbNSFP2.0b3.chr1_69134.txt", vcfFileName };
 		SnpSiftCmdAnnotateSortedDbNsfp cmd = new SnpSiftCmdAnnotateSortedDbNsfp(args);
-		cmd.setVerbose(true);
+		cmd.setVerbose(verbose);
+		cmd.setDebug(debug);
 
 		try {
 			cmd.initAnnotate();
@@ -31,7 +35,7 @@ public class TestCasesDbNsfp extends TestCase {
 			VcfEntry vcfEntry = vcfFile.next();
 
 			cmd.annotate(vcfEntry);
-			Gpr.debug(vcfEntry);
+			if (debug) Gpr.debug(vcfEntry);
 
 			// Check all values
 			Assert.assertEquals("2.31", vcfEntry.getInfo(SnpSiftCmdAnnotateSortedDbNsfp.VCF_INFO_PREFIX + "GERP++_RS"));
@@ -48,4 +52,39 @@ public class TestCasesDbNsfp extends TestCase {
 			throw new RuntimeException(e);
 		}
 	}
+
+	/**
+	 * Test dbnsfp having multiple lines per variant
+	 */
+	public void test_02() {
+		String vcfFileName = "test/test_dbnsfp_multiple.vcf";
+		String fields = "genename,Ensembl_geneid,Ensembl_transcriptid";
+		String args[] = { "-f", fields, "test/test_dbnsfp_multiple_lines.txt", vcfFileName };
+
+		SnpSiftCmdAnnotateSortedDbNsfp cmd = new SnpSiftCmdAnnotateSortedDbNsfp(args);
+		cmd.setVerbose(verbose);
+		cmd.setDebug(debug);
+
+		try {
+			cmd.initAnnotate();
+
+			// Get entry.
+			// Note: There is only one entry to annotate (the VCF file has one line)
+			VcfFileIterator vcfFile = new VcfFileIterator(vcfFileName);
+			VcfEntry vcfEntry = vcfFile.next();
+
+			cmd.annotate(vcfEntry);
+			if (debug) Gpr.debug(vcfEntry);
+
+			// Check all values
+			Assert.assertEquals("ENST00000368485,ENST00000515190", vcfEntry.getInfo(SnpSiftCmdAnnotateSortedDbNsfp.VCF_INFO_PREFIX + "Ensembl_transcriptid"));
+			Assert.assertEquals("IL6R", vcfEntry.getInfo(SnpSiftCmdAnnotateSortedDbNsfp.VCF_INFO_PREFIX + "genename"));
+			Assert.assertEquals("ENSG00000160712", vcfEntry.getInfo(SnpSiftCmdAnnotateSortedDbNsfp.VCF_INFO_PREFIX + "Ensembl_geneid"));
+
+			cmd.endAnnotate();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
