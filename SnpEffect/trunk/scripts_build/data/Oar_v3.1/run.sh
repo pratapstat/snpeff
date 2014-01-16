5b50916f23990a30f9ded82652d958f618f94e28
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2014-01-16 21:14:55 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@1166 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpEffect/trunk/scripts_build/data/Oar_v3.1/run.sh
+++ SnpEffect/trunk/scripts_build/data/Oar_v3.1/run.sh
@@ -45,50 +45,50 @@
 # Create reference genome FASTA file
 #---
 
-# zcat \
-# 		ORI/oar_ref_Oar_v3.1_chr1.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr2.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr3.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr4.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr5.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr6.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr7.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr8.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr9.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr10.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr11.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr12.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr13.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr14.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr15.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr16.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr17.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr18.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr19.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr20.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr21.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr22.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr23.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr24.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr25.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chr26.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chrMT.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_chrX.fa.gz \
-# 		ORI/oar_ref_Oar_v3.1_unplaced.fa.gz \
-# 	| sed "s/^>gi|.*|ref|\(N._.*\)|.*/>\1/" \
-# 	> Oar_v3.1.fa
-# pigz Oar_v3.1.fa
-# mv Oar_v3.1.fa.gz ../genomes/
-# 
+zcat \
+		ORI/oar_ref_Oar_v3.1_chr1.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr2.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr3.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr4.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr5.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr6.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr7.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr8.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr9.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr10.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr11.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr12.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr13.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr14.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr15.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr16.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr17.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr18.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr19.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr20.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr21.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr22.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr23.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr24.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr25.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chr26.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chrMT.fa.gz \
+		ORI/oar_ref_Oar_v3.1_chrX.fa.gz \
+		ORI/oar_ref_Oar_v3.1_unplaced.fa.gz \
+	| sed "s/^>gi|.*|ref|\(N._.*\)|.*/>\1/" \
+	> Oar_v3.1.fa
+pigz Oar_v3.1.fa
+mv Oar_v3.1.fa.gz ../genomes/
+
 
 #---
 # Create genes list
 #---
 
-# zcat ORI/ref_Oar_v3.1_top_level.gff3.gz > genes.gff
+zcat ORI/ref_Oar_v3.1_top_level.gff3.gz > genes.gff
 
-# grep "Parent=rna" genes.gff | grep CDS | cut -f 9 | tr ";" "\t" | cut -f 2,3 | tr "=" "\t" | cut -f 2,4 | uniq > replace_list.txt
-# cat genes.gff | ./replace.pl > genes.new.gff
+grep "Parent=rna" genes.gff | grep CDS | cut -f 9 | tr ";" "\t" | cut -f 2,3 | tr "=" "\t" | cut -f 2,4 | uniq > replace_list.txt
+cat genes.gff | ./replace.pl > genes.new.gff
 mv genes.new.gff genes.gff
 
 
@@ -96,8 +96,8 @@ mv genes.new.gff genes.gff
 # Create proteins list
 #---
 
-# zcat ORI/protein.fa.gz \
-# 	| sed "s/^>gi|.*|ref|\(.._.*\)|.*/>\1/" \
-# 	> protein.fa
+zcat ORI/protein.fa.gz \
+	| sed "s/^>gi|.*|ref|\(.._.*\)|.*/>\1/" \
+	> protein.fa
 
 
