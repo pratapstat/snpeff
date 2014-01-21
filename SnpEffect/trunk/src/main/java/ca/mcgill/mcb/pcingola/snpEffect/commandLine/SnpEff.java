2269c75c9c23677311d2a4e534778cee30fc559b
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2014-01-21 22:09:05 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@1172 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpEffect/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpEffect/commandLine/SnpEff.java
+++ SnpEffect/trunk/src/main/java/ca/mcgill/mcb/pcingola/snpEffect/commandLine/SnpEff.java
@@ -47,8 +47,8 @@ public class SnpEff implements CommandLine {
 
 	// Version info
 	public static final String SOFTWARE_NAME = "SnpEff";
-	public static final String REVISION = "i";
-	public static final String BUILD = "2014-01-16";
+	public static final String REVISION = "j";
+	public static final String BUILD = "2014-01-21";
 	public static final String VERSION_MAJOR = "3.4";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
 	public static final String VERSION_NO_NAME = VERSION_SHORT + " (build " + BUILD + "), by " + Pcingola.BY;
