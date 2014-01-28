de65115102eb2213e48fabd316f6d9582c1f6246
pcingola <pcingola@3341ced4-5d85-40e2-8a85-8d6cb0b68e6b>
2014-01-28 23:11:16 +0000

    git-svn-id: https://svn.code.sf.net/p/snpeff/code@1184 3341ced4-5d85-40e2-8a85-8d6cb0b68e6b

--- SnpEffect/trunk/scripts_build/testCases.sh
+++ SnpEffect/trunk/scripts_build/testCases.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd $HOME/workspace/SnpSift/
-java -Xmx4g -cp SniSift.jar ca.mcgill.mcb.pcingola.snpSift.testCases.TestSuiteAll 2>&1 | tee testSuiteAll.snpsift.txt
+java -Xmx4g -cp $HOME/snpEff/SnpSift.jar ca.mcgill.mcb.pcingola.snpSift.testCases.TestSuiteAll 2>&1 | tee testSuiteAll.snpsift.txt
 
 cd $HOME/workspace/SnpEff/
-java -Xmx4g -cp snpEff.jar ca.mcgill.mcb.pcingola.snpEffect.testCases.TestSuiteAll 2>&1 | tee testSuiteAll.snpeff.txt
+java -Xmx4g -cp $HOME/snpEff/snpEff.jar ca.mcgill.mcb.pcingola.snpEffect.testCases.TestSuiteAll 2>&1 | tee testSuiteAll.snpeff.txt
 
